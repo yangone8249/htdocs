0de8249143f442db62ff17969168ae7be55eaf37
yangone8249 <yangone8249@gmail.com>
2024-03-25 09:28:22 +0900

    test

---
+++ Untitled-1.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo "PHP 실습과제입니다.";
+echo "저는 양휘원입니다. 집은 인천 남동구 입니다.";
+
+?>
+</body>
+</html>
